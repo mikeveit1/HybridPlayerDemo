7d13d8d9394e3f5458f151c1c7f7ab3599d6de53
mikeveit1 <mikeveit1@gmail.com>
2025-07-11 19:09:19 -0400

    fixed app delegate for old arch change

--- ios/HybridPlayerDemo/AppDelegate.swift
+++ ios/HybridPlayerDemo/AppDelegate.swift
@@ -1,48 +1,43 @@
 import UIKit
 import React
-import React_RCTAppDelegate
-import ReactAppDependencyProvider
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
-
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
+    
+    let jsCodeLocation: URL
+    
+    #if DEBUG
+    guard let debugURL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index") else {
+      fatalError("Could not load JavaScript bundle from Metro server")
+    }
+    jsCodeLocation = debugURL
+    #else
+    guard let bundleURL = Bundle.main.url(forResource: "main", withExtension: "jsbundle") else {
+      fatalError("Could not find JavaScript bundle in app bundle")
+    }
+    jsCodeLocation = bundleURL
+    #endif
 
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "HybridPlayerDemo",
-      in: window,
+    let rootView = RCTRootView(
+      bundleURL: jsCodeLocation,
+      moduleName: "HybridPlayerDemo",
+      initialProperties: nil,
       launchOptions: launchOptions
     )
 
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+
     return true
-  }
-}
-
-class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
-
-  override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
   }
 }
